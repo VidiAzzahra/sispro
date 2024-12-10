3728653ed132222b827356d6f22b08c899ec40bc
VidiAzzahra <vidiazzahra04@gmail.com>
2024-12-10 15:04:45 +0700

    update

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(){
+        if (Auth::check()) {
+            return redirect('home');
+        }else{
+            return view('login');
+        }
+    }
+
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/');
+    }
+}
